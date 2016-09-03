99252398bb0dbb5cd881eea7e653253b852e23fa
Felix Wagner <felix.wagner@clarity-ag.net>
2016-09-03 20:12:03 +0200

    Completed switch to ASP.NET Core 1.0 Added ASP.NET Core MVC Got running Hello World!

---
+++ WebChat2/WebChat2/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebChat2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebChat2/WebChat2/app/App.js
@@ -0,0 +1,13 @@
+﻿import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class Hello extends Component {
+    render() {
+        return(
+            <h1>Hello World</h1>
+        )
+    };
+}
+
+
+render(<Hello />, document.getElementById("root"));
